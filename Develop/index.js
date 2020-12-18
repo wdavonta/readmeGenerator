b79ccc8cf1a59bbb5a687506b38ca3cc33fa9f60
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2020-12-17 23:03:04 -0500

    added package and readme function

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,20 @@
 // TODO: Include packages needed for this application
+const fs =require('fs');
+const inquirer = require('inquirer');
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('readme been created')
+});
+}
 
 // TODO: Create a function to initialize app
 function init() {}
